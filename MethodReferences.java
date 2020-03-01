e1bf9757cc45569273e4ba874564a6bfc09e4cb5
AMRITPK <AMRITPK@GMAIL.COM>
2020-03-01 14:23:27 +0530

    Create MethodReferences.java

---
+++ MethodReferences.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+
+public class MethodReferences {
+	
+	public static String testStatic () {
+		
+	    System.out.println("Hello, this is static method being used as the method passed to callable's functional interface(abstr method)");  
+	    return "ret1";
+	}
+	public static void main (String args[]) throws InterruptedException, ExecutionException {
+		ExecutorService exs= Executors.newFixedThreadPool(3);
+		Future f1 = exs.submit(MethodReferences::testStatic);
+		System.out.println(f1.get());
+		exs.shutdown();
+	}
+	
+	
+}
+
